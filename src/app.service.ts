acc25eec5c42a10e4b80b67964d77da2923bd5e5
polartar <polaris.center@outlook.com>
2023-04-29 16:47:58 +0530

    pipeline check

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World ! Welcome to vtvl';
+    return 'Hello World !';
   }
 
   getHelloName(name: string): string {
