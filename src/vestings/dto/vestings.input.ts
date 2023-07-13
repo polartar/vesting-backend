b99a47331c4bb8dfcb70e13c47bcc98b301846ba
polartar <polaris.center@outlook.com>
2023-07-12 22:54:14 -0500

    fix vesting creation payload validation (#69)

--- src/vestings/dto/vestings.input.ts
+++ src/vestings/dto/vestings.input.ts
@@ -50,66 +50,79 @@ export class CreateVestingRecipeInput {
 export class CreateVestingDetailInput {
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => String)
   organizationId: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => String)
   vestingContractId: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => String)
   tokenId: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => String)
   name: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => Date)
   startedAt: Date;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => Date)
   endedAt: Date;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Field(() => Date)
   originalEndedAt: Date;
 
   @ApiProperty()
   @IsEnum(ReleaseFrequencyType)
+  @IsNotEmpty()
   @Field(() => ReleaseFrequencyType)
   releaseFrequencyType: ReleaseFrequencyType;
 
   @ApiProperty()
   @IsNumber()
+  @IsNotEmpty()
   @Field(() => Number)
   releaseFrequency: number;
 
   @ApiProperty()
   @IsEnum(CliffDurationType)
+  @IsNotEmpty()
   @Field(() => CliffDurationType)
   cliffDurationType: CliffDurationType;
 
   @ApiProperty()
   @IsNumber()
+  @IsNotEmpty()
   @Field(() => Number)
   cliffDuration: number;
 
   @ApiProperty()
   @IsNumberString()
+  @IsNotEmpty()
   @Field(() => String)
   cliffAmount: string;
 
   @ApiProperty()
   @IsNumberString()
+  @IsNotEmpty()
   @Field(() => String)
   amount: string;
 }
