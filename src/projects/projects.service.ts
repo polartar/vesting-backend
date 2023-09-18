ef8d1451863dc519c848f4406e185d73e50e9506
polartar <polaris.center@outlook.com>
2023-09-18 11:31:26 -0700

    feat: integrate slack webhook for project creation

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,10 +2,38 @@ import { PrismaService } from 'nestjs-prisma';
 import { Injectable } from '@nestjs/common';
 import { CreateProjectInput, QueryProjectInput } from './dto/project.input';
 import { Project } from '@prisma/client';
+import axios from 'axios';
 
 @Injectable()
 export class ProjectsService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async notifySlack(organizationId: string, name: string) {
+    try {
+      await axios.post(process.env.SLACK_NEW_PROJECT_WEBHOOK_URL, {
+        username: organizationId, // This will appear as user name who posts the message
+        text: `Project (${name}) just created`,
+        icon_emoji: ':new:', // User icon, you can also use custom icons here
+        attachments: [
+          {
+            color: '#eed140', // color of the attachments sidebar.
+            fields: [
+              {
+                title: 'Project Name',
+                value: name,
+                short: true,
+              },
+              {
+                title: 'Organization',
+                value: organizationId,
+                short: true,
+              },
+            ],
+          },
+        ],
+      });
+    } catch (err: any) {}
+  }
 
   async create({ entityIds, ...data }: CreateProjectInput) {
     const project = await this.prisma.project.create({
@@ -16,6 +44,7 @@ export class ProjectsService {
         wallet: data.wallet ?? '',
       },
     });
+    await this.notifySlack(data.organizationId, data.name);
 
     await this.prisma.projectEntity.createMany({
       data: entityIds.map((entityId) => ({
