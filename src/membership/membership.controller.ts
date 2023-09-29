e50c8a8f3f5c47b8e4f803f2fbdd373595bf020a
polartar <polaris.center@outlook.com>
2023-09-29 06:23:21 -0700

    feat: membership delete

--- src/membership/membership.controller.ts
+++ src/membership/membership.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MembershipService } from './membership.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { OrganizationFounderAuth } from 'src/common/utils/auth';
@@ -24,5 +32,13 @@ export class MembershipsController {
   @Get('/organizationId/:organizationId')
   async getMemberships(@Param('organizationId') organizationId: string) {
     return await this.membership.getMemberShips(organizationId);
+  }
+
+  @ApiBearerAuth()
+  @OrganizationFounderAuth()
+  @UseGuards(GlobalAuthGuard)
+  @Delete('/:organizationId/:membershipId')
+  async deleteMembership(@Param('membershipId') membershipId: string) {
+    return await this.membership.deleteMembership(membershipId);
   }
 }
